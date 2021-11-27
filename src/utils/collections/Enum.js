2fba7311fc95a825434199de5ccc1be076287cae
German Bilalov <pinncho@gmail.com>
2021-11-27 23:52:09 +0600

    feat: add support for custom Enum values

--- src/utils/collections/Enum.js
+++ src/utils/collections/Enum.js
@@ -1,9 +1,23 @@
 export default class Enum {
     constructor(...keys) {
-        keys.forEach((key) => {
-            this[key] = key;
-        });
+        if (typeof keys[0] === 'string') {
+            this.addKeys(keys);
+        } else if (typeof keys[0] === 'object') {
+            this.addObjectKeys(keys[0]);
+        }
         Object.freeze(this);
+    }
+
+    addKeys(keys) {
+        keys.forEach((key) => this.addKey(key));
+    }
+
+    addObjectKeys(obj) {
+        Object.entries(obj).forEach(([key, value]) => this.addKey(key, value));
+    }
+
+    addKey(key, value) {
+        value ? (this[key] = value) : (this[key] = key);
     }
 
     *[Symbol.iterator]() {
